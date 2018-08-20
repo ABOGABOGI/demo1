2aed4844be98cdc3330341469540eafb92a3d188
Dhanang Haryo <dhanangwicaksono6@gmail.com>
2018-08-20 13:21:58 +0700

    add login use gmail

--- app/src/main/java/com/solarnet/demo/activity/login/LoginAcitivty.kt
+++ app/src/main/java/com/solarnet/demo/activity/login/LoginAcitivty.kt
@@ -1,45 +1,67 @@
 package com.solarnet.demo.activity.login
 
-import android.annotation.SuppressLint
+
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.InputType
+import android.util.Log
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.common.api.Status
 import com.solarnet.demo.MainActivity
 import com.solarnet.demo.R
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginAcitivty : AppCompatActivity(){
+class LoginAcitivty : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener{
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d("bett", "onConnectionFailed:" + connectionResult);
+    }
+
+    private val RC_SIGN_IN = 9001
+    private var mGoogleApiClient: GoogleApiClient? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+      var btn_logins = findViewById(R.id.btn_submit) as Button
 
-        var et_user_name = findViewById(R.id.et_user_name) as EditText
-        var et_password = findViewById(R.id.et_password) as EditText
-        var btn_submit = findViewById(R.id.btn_submit) as Button
-        var txt_signup = findViewById(R.id.sign_up_edittext) as TextView
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
 
-        // set on-click listener
-        btn_submit.setOnClickListener {
-            val user_name = et_user_name.text;
-            val password = et_password.text;
-//            Toast.makeText(this@LoginAcitivty, user_name, Toast.LENGTH_LONG).show()
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
 
-            if (user_name.trim().length > 0 && password.trim().length > 0){
-                startActivity(Intent(this@LoginAcitivty, MainActivity::class.java))
-            }else{
-                Toast.makeText(applicationContext, "Please check your username and password ", Toast.LENGTH_SHORT).show()
-            }
+        btn_logins?.setOnClickListener(View.OnClickListener {
+            val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+//            startActivity(Intent(this@LoginAcitivty, MainActivity::class.java))
+        })
+    }
 
-            txt_signup.setOnClickListener {
-                Toast.makeText(this@LoginAcitivty, "Please Sign in our Website", Toast.LENGTH_LONG).show()
-            }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+//            handleSignInResult(result)
+            startActivity(Intent(this@LoginAcitivty, MainActivity::class.java))
+        }
+    }
+
+        private fun handleSignInResult(result: GoogleSignInResult) {
+        if (result.isSuccess) {
+            startActivity(Intent(this@LoginAcitivty, MainActivity::class.java))
         }
     }
 }
