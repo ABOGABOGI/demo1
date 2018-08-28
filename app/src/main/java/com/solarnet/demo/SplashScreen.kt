4f4030b96cc653aa427380d0629cf86bc9eec6f6
Dhanang Haryo <dhanangwicaksono6@gmail.com>
2018-08-28 17:19:38 +0700

    add intro

--- app/src/main/java/com/solarnet/demo/SplashScreen.kt
+++ app/src/main/java/com/solarnet/demo/SplashScreen.kt
@@ -4,13 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import com.solarnet.demo.activity.login.LoginAcitivty
+import com.solarnet.demo.activity.login.IntroActivity
+import com.solarnet.demo.activity.login.PrefManager
 
 class SplashScreen : AppCompatActivity(){
 
     val handler = Handler()
+    private var prefManager: PrefManager? = null
     val runnable = Runnable {
-        startActivity(Intent(this@SplashScreen, LoginAcitivty::class.java))
+        prefManager = PrefManager(this)
+        if (!prefManager!!.isFirstTimeLaunch()) {
+            startActivity(Intent(this@SplashScreen, IntroActivity::class.java))
+            finish()
+        }else{
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }
+
+//        startActivity(Intent(this@SplashScreen, IntroActivity::class.java))
     }
 
     override fun onResume() {
