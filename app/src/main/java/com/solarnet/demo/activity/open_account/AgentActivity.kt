f2375e849d4c99f9043d22d419af26491a65abbe
Dhanang Haryo <dhanangwicaksono6@gmail.com>
2018-09-05 00:03:09 +0700

    fix27

---
+++ app/src/main/java/com/solarnet/demo/activity/open_account/AgentActivity.kt
@@ -0,0 +1,15 @@
+package com.solarnet.demo.activity.open_account
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.solarnet.demo.R
+
+class AgentActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_agen)
+
+    }
+
+}
